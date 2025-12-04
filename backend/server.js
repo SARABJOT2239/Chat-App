70426bd5de1eb50f66c6f11e2c62cc4644b60f85
sarabjot1609 <sarabjot1609@gmail.com>
2025-12-04 11:56:11 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
@@ -9,7 +10,7 @@ import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with json payloads (from req.body)
@@ -19,6 +20,10 @@ app.use("/api/auth" ,authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // app.get("/", (req, res) => {
 //     //root route http://localhost:5000/
 //     res.send("hello world!!!");
